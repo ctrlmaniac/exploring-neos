253127d6f3d5f0ab61e5446a79a15015026b3502
Davide Di Criscito <davide.dicriscito@gmail.com>
2022-06-22 18:57:58 +0200

    write output files

--- write.py
+++ write.py
@@ -12,6 +12,7 @@ You'll edit this file in Part 4.
 """
 import csv
 import json
+from math import isnan
 
 
 def write_to_csv(results, filename):
@@ -25,10 +26,38 @@ def write_to_csv(results, filename):
     :param filename: A Path-like object pointing to where the data should be saved.
     """
     fieldnames = (
-        'datetime_utc', 'distance_au', 'velocity_km_s',
-        'designation', 'name', 'diameter_km', 'potentially_hazardous'
+        "datetime_utc",
+        "distance_au",
+        "velocity_km_s",
+        "designation",
+        "name",
+        "diameter_km",
+        "potentially_hazardous",
     )
-    # TODO: Write the results to a CSV file, following the specification in the instructions.
+
+    results_list = list()
+
+    for result in results:
+        results_list.append(
+            {
+                "datetime_utc": result.time_str,
+                "distance_au": result.distance,
+                "velocity_km_s": result.velocity,
+                "designation": result._designation,
+                "name": result.neo.name,
+                "diameter_km": ""
+                if isnan(result.neo.diameter)
+                else result.neo.diameter,
+                "potentially_hazardous": result.neo.hazardous,
+            }
+        )
+
+    with open(filename, "w") as outfile:
+        writer = csv.DictWriter(outfile, fieldnames)
+        writer.writeheader()
+
+        for result in results_list:
+            writer.writerow(result)
 
 
 def write_to_json(results, filename):
@@ -42,4 +71,24 @@ def write_to_json(results, filename):
     :param results: An iterable of `CloseApproach` objects.
     :param filename: A Path-like object pointing to where the data should be saved.
     """
-    # TODO: Write the results to a JSON file, following the specification in the instructions.
+    results_list = list()
+
+    for result in results:
+        results_list.append(
+            {
+                "datetime_utc": result.time_str,
+                "distance_au": result.distance,
+                "velocity_km_s": result.velocity,
+                "neo": {
+                    "designation": result.neo.designation,
+                    "name": result.neo.name,
+                    "diameter_km": ""
+                    if isnan(result.neo.diameter)
+                    else result.neo.diameter,
+                    "potentially_hazardous": result.neo.hazardous,
+                },
+            }
+        )
+
+    with open(filename, "w") as outfile:
+        json.dump(results_list, outfile, indent=2)
